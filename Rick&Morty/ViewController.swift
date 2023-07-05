ff19f2ea36a2ce826fc9994aec0bf64002717140
Artur Peplinski <art.peplinski@gmail.com>
2023-07-05 17:07:21 +0200

    Initial Commit

---
+++ Rick&Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&Morty
+//
+//  Created by Artur Peplinski on 05/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
